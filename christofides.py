30c892713dc9656dbfad79e93d1d6c3c97186980
Noah D'Souza <ndsouza@olin.edu>
2019-12-05 22:44:36 -0500

    Now works and outputs things. They are not correct.

--- christofides.py
+++ christofides.py
@@ -16,23 +16,30 @@ def christofides(graph: 'Graph'):
     edges = [(n1.point,n2.point,dist(n1,n2)) for n1,n2 in graph.get_edges()]
         # 'edges' is technically a graph, don't worry
     MST_tree = find_mst2(graph)
-    odd_verts = find_odd_verts(MST_tree)
-    print(MST_tree)
+    # odd_verts = find_odd_verts(MST_tree)
+    # print(MST_tree)
     MST_tree = minimum_weight_match(MST_tree)
     euc = euler_circ(MST_tree, graph)
-    curr = euc[0]
+    # curr = euc[0]
+    curr = 0
     path = [curr]
     visited = [False]*len(euc)
     visited[0] = True
     length = 0
+    # TODO: so 'i' is a tuple, which obviously you can't index with....
+    print(euc)
+    euc_temp = euc.copy()
     for i in euc[1:]:
-        if not visited[i]:
+        temp_ind = euc_temp.index(i)
+        if not visited[temp_ind]:
+            euc_temp[temp_ind] = ' '
             path.append(i)
-            visited[i] = True
-            length += G[curr][i]
+            visited[temp_ind] = True
+            # print(edges[curr][temp_ind])
+            # length += edges[curr][2]
             curr = i
     path.append(path[0])
-    return length, path
+    return path
 
 def find_odd_verts(mst):
     temp = {}
@@ -68,10 +75,12 @@ def euler_circ(matched_mst, uhh):
         while len(nabes[v]) > 0:
             w = nabes[v][0]
             edge_remove(matched_mst, v, w)
-            del nabes[v][(nabes[w].index(v))]
+            del nabes[v][(nabes[v].index(w))]
+            del nabes[w][(nabes[w].index(v))]
             i = i+1
             euc.insert(i,w)
             v = w
+    # print(type(euc))
     return euc
 
 def edge_remove(matched_mst, v1, v2):
@@ -82,15 +91,35 @@ def edge_remove(matched_mst, v1, v2):
 
 
 if __name__ == '__main__':
-    d = [
-        [(0, 0), (1, 1)],  # simple two-point line
-        [(0, 0), (0, 1), (1, 2), (2, 3), (0, 0)],  # a cycle
-        [(1, 2), (4, 5), (2, 3)],  # a bent three-point line
-    ]
+    # d = [
+    #     [(0, 0), (1, 1)],  # simple two-point line
+    #     [(0, 0), (0, 1), (1, 2), (2, 3), (0, 0)],  # a cycle
+    #     [(1, 2), (4, 5), (2, 3)],  # a bent three-point line
+    # ]
+    d = [[(1380, 939), (2848, 96), (3510, 1671), (457, 334), (3888, 666),
+        (984, 965), (2721, 1482), (1286, 525), (2716, 1432), (738, 1325),
+        (1251, 1832), (2728, 1698), (3815, 169), (3683, 1533), (1247, 1945)]]
+        # (123, 862), (1234, 1946), (252, 1240), (611, 673), (2576, 1676),
+        # (928, 1700), (53, 857), (1807, 1711), (274, 1420), (2574, 946),
+        # (178, 24), (2678, 1825), (1795, 962), (3384, 1498), (3520, 1079),
+        # (1256, 61), (1424, 1728), (3913, 192), (3085, 1528), (2573, 1969),
+        # (463, 1670), (3875, 598), (298, 1513), (3479, 821), (2542, 236),
+        # (3955, 1743), (1323, 280), (3447, 1830), (2936, 337), (1621, 1830),
+        # (3373, 1646), (1393, 1368), (3874, 1318), (938, 955), (3022, 474),
+        # (2482, 1183), (3854, 923), (376, 825), (2519, 135), (2945, 1622),
+        # (953, 268), (2628, 1479), (2097, 981), (890, 1846), (2139, 1806),
+        # (2421, 1007), (2290, 1810), (1115, 1052), (2588, 302), (327, 265),
+        # (241, 341), (1917, 687), (2991, 792), (2573, 599), (19, 674),
+        # (3911, 1673), (872, 1559), (2863, 558), (929, 1766), (839, 620),
+        # (3893, 102), (2178, 1619), (3822, 899), (378, 1048), (1178, 100),
+        # (2599, 901), (3416, 143), (2961, 1605), (611, 1384), (3113, 885),
+        # (2597, 1830), (2586, 1286), (161, 906), (1429, 134), (742, 1025),
+        # (1625, 1651), (1187, 706), (1787, 1009), (22, 987), (3640, 43),
+        # (3756, 882), (776, 392), (1724, 1642), (198, 1810), (3950, 1558)]
     g = Graph.from_drawing(d)
-    length, path = christofides(g)
+    path = christofides(g)
     print(path)
-    print(length)
+    # print(length)
 
 
 
