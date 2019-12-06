9d7518af2c66df620f9969f3c7043457e99c2136
bwerth <bryan.werth@students.olin.edu>
2019-12-05 19:52:02 -0500

    Fixed distance problem

--- blossom.py
+++ blossom.py
@@ -13,7 +13,10 @@ import random
 #   - distance_to():
 #       - returns distance to a node (weight?)
 
-def minimum_weight_match(mst):
+def dist_verts(u,v):
+	return math.sqrt((u[0] - v[0])**2 + (u[1] - v[1])**2)
+
+def minimum_weight_match(mst,graph):
 	odd_vertices = find_odd_verts(mst)
 	random.shuffle(odd_vertices)
 	while odd_vertices:
@@ -21,18 +24,20 @@ def minimum_weight_match(mst):
 		length = float("inf")
 		closest = 0
 		for u in odd_vertices:
-			if v != u and v.distance_to(u) < length:
-				length = v.distance_to(u)
+			if(v != u and dist_verts(u,v) < length):
+				length = dist_verts(u,v)
 				closest = u
 		is_in_mst = 0
 		for edge in mst:
-			if(edge[2] is v.distance_to(u)):
+			if(edge[2] is dist_verts(u,v)):
 				if(edge[0],edge[1] is u,v or edge[0],edge[1] is v,u):
 					is_in_mst = 1
 		if(not is_in_mst):
-			mst.add((u,v,v.distance_to(u)))
+			mst.add(dist_verts(u,v))
 		odd_vertices.remove(u)
 	return mst
+
+
 
 
 
